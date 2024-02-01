4811b8746753765f32f9e03d7e5896398f3996ce
THIRUHARIKARAN R <105100368+AzharPredator@users.noreply.github.com>
2024-02-01 13:36:36 +0000

    tharun 01-02-2024

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -3,7 +3,30 @@ pragma solidity ^0.8.0;
 
 contract Certificate {
     struct AcademicCertificate {
-        string studentName;
+
+//unit256 folio no;//
+        string nameOfCandidate;
+        unit256 folioNo;
+        unit256 registerNo;
+        string memory dateOfBirth;
+        string gender;
+        string month&YearOfExamination;
+        string dateOfPublication;
+        string programme&Branch;
+        string bTechInformationTechnology;
+        string regulation;
+        uint256 semNo;
+        string courseCode;
+        string attCode;
+        string letterGrade;
+        unit256 gradePoint;
+        string courseTitle;
+        uint256 credits;
+        string result;
+        string semester;
+        unit256 creditsRegistered;
+        unit256 creditsEarned;
+
         string degree;
         uint256 graduationDate;
         bool isIssued;
@@ -11,17 +34,64 @@ contract Certificate {
 
     mapping(address => AcademicCertificate) public certificates;
 
-    event CertificateIssued(address indexed student, string studentName, string degree, uint256 graduationDate);
+    event CertificateIssued(address indexed student, string nameOfCandidate, unit256 folioNo, unit256 registerNo,
+     string memory dateOfBirth, string gender, string month&YearOfExamination, string dateOfPublication, string programme&Branch,
+      string bTechInformationTechnology, string regulation, uint256 semNo, string courseCode, string attCode, string letterGrade, unit256 gradePoint,
+       string courseTitle, uint256 credits, string result, string semester, unit256 creditsEarned,
+
+     string degree, uint256 graduationDate);
 
     function issueCertificate(
-        string memory studentName,
+        string memory nameOfCandidate,
+        unit256 folioNo,
+        unit256 registerNo,
+        string memory dateOfBirth,
+        string gender,
+        string month&YearOfExamination,
+        string dateOfPublication,
+        string programme&Branch,
+        string bTechInformationTechnology,
+        string regulation,
+        uint256 semNo,
+        string courseCode,
+        string attCode,
+        string letterGrade,
+        unit256 gradePoint,
+        string courseTitle,
+        uint256 credits,
+        string result,
+        string semester,
+        unit256 creditsRegistered,
+        unit256 creditsEarned,
+
         string memory degree,
         uint256 graduationDate
     ) public {
         require(!certificates[msg.sender].isIssued, "Certificate already issued");
 
         AcademicCertificate memory newCertificate = AcademicCertificate({
-            studentName: studentName,
+            nameOfCandidate: nameOfCandidate,
+            folioNo: folioNo,
+            registerNo: registerNo,
+            dateOfBirth: dateOfBirth,
+            gender: gender,
+            month&YearOfExamination: month&YearOfExamination,
+            dateOfPublication: dateOfPublication,
+            programme&Branch: programme&Branch,
+            bTechInformationTechnology: bTechInformationTechnology,
+            regulation: regulation,
+            semNo: semNo,
+            courseCode: courseCode,
+            attCode: attCode,
+            letterGrade: letterGrade,
+            gradePoint: gradePoint,
+            courseTitle: courseTitle,
+            credits: credits,
+            result: result,
+            semester: semester,
+            creditsRegistered: creditsRegistered,
+            creditsEarned: creditsEarned,
+
             degree: degree,
             graduationDate: graduationDate,
             isIssued: true
@@ -29,11 +99,25 @@ contract Certificate {
 
         certificates[msg.sender] = newCertificate;
 
-        emit CertificateIssued(msg.sender, studentName, degree, graduationDate);
+        emit CertificateIssued(msg.sender, nameOfCandidate, folioNo, registerNo, dateOfBirth, gender, month&YearOfExamination, dateOfPublication,
+         programme&Branch, bTechInformationTechnology, regulation, semNo, courseCode, attCode, letterGrade, gradePoint, courseTitle, credits,
+          result, semester, creditsRegistered, creditsEarned,
+
+         degree, graduationDate);
     }
 
-    function getCertificateDetails(address student) public view returns (string memory, string memory, uint256, bool) {
+    function getCertificateDetails(address student) public view returns (string memory, uint256, uint256,
+     string memory, string memory, string memory, string memory, string memory, string memory, string memory, uint256, string memory, string memory,
+      string memory, uint256, string memory, uint256, string memory, string memory, uint256, uint256,
+
+     string memory, uint256, bool) {
         AcademicCertificate memory certificate = certificates[student];
-        return (certificate.studentName, certificate.degree, certificate.graduationDate, certificate.isIssued);
+        return (certificate.nameOfCandidate, certificate.folioNo, certificate.registerNo, certificate.dateOfBirth,
+         certificate.gender, certificate.month&YearOfExamination, certificate.dateOfPublication, certificate.programme&Branch, 
+         certificate.bTechInformationTechnology, certificate.regulation, certificate.semNo, certificate.courseCode, certificate.attCode, 
+         certificate.letterGrade, certificate.gradePoint, certificate.courseTitle, certificate.credits, certificate.result, certificate.semester,
+          certificate.creditsRegistered, certificate.creditsEarned,
+         
+         certificate.degree, certificate.graduationDate, certificate.isIssued);
     }
 }
